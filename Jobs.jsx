6f9590586dc43829204ae6b727be4e712ffd82d8
Mason Hendrick <super40111@gmail.com>
2020-12-10 19:17:00 -0800

    Delete Jobs.jsx

--- Jobs.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import{Form, Button, Card} from 'react-bootstrap'
-
-export default function Jobs(props) {
-    return (
-        <Form.Control as="Textarea" rows={1}/>
-    )
-}
